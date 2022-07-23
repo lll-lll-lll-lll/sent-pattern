5ef7b4441bf7286dbab6fa420e5e8a423eecd3a9
0xpeIpeI <hokumaru12@gmail.com>
2022-07-23 21:36:11 +0900

    change en_core_web_lg to en_core_web_sm

--- examples/test_env/main.py
+++ examples/test_env/main.py
@@ -1,5 +1,7 @@
 import spacy
-nlp = spacy.load("en_core_web_lg")
+nlp = spacy.load("en_core_web_sm")
 nlp.add_pipe("sent_pattern")
 doc = nlp("I like yuo")
+pattern = doc._.sentpattern
 print(doc._.sentpattern.subject.root.text)
+print(pattern.abbreviation)
