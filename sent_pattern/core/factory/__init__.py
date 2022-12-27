423b76b1287caf801986d3bb53cc85f65370317b
0xpeIpeI <hokumaru12@gmail.com>
2022-12-27 13:34:18 +0900

    solve module not found

---
+++ sent_pattern/core/factory/__init__.py
@@ -0,0 +1,5 @@
+from .elements import RootElements as RootElements
+from .elements import CustomElements as CustomElements
+from .elements import ElementsFactory as ElementsFactory
+
+from .pattern import SentencePattern as SentencePattern
