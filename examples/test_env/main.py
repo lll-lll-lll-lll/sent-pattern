edcb8028d04b6150aa18a2bbf0fec8bc086e189a
0xpeIpeI <hokumaru12@gmail.com>
2022-12-29 03:28:23 +0900

    add: test get extesion method

--- examples/test_env/main.py
+++ examples/test_env/main.py
@@ -1,7 +1,7 @@
 import spacy
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_lg")
 nlp.add_pipe("sent_pattern")
-doc = nlp("I like yuo")
+doc = nlp("The Eureka client handles all aspects of service instance registration and deregistration")
+get_ext_tuple  = doc.get_extension("sentpattern")
 pattern = doc._.sentpattern
 print(doc._.sentpattern.subject.root.text)
-print(pattern.abbreviation)
