aaf51ab73a8629157d733e618e8e485b034f9312
0xpeIpeI <hokumaru12@gmail.com>
2023-02-03 20:24:49 +0900

    add subject, verb object

--- sent_pattern/core/interface/Ipattern.py
+++ sent_pattern/core/interface/Ipattern.py
@@ -1,10 +1,21 @@
 from abc import ABCMeta, abstractmethod
 from typing import List,Dict
 from spacy.tokens import Token
+from sent_pattern.core.elements import Subject,Verb
 
 class IBaseSentencePattern(metaclass=ABCMeta):
     """ All FifthSentencePattern Base Interface
     """
+    @property
+    @abstractmethod
+    def subject(self) -> Subject:
+        raise NotImplementedError()
+    
+    @property
+    @abstractmethod
+    def verb(self) -> Verb:
+        raise NotImplementedError()
+    
     @property
     @abstractmethod
     def spans(self) -> Dict[str, List[Token]]:
