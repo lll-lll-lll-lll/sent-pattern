81b5158d15e96d766de74978523d2384b928096b
0xpeIpeI <hokumaru12@gmail.com>
2023-02-04 22:18:10 +0900

    analyze multiple sentences at once

--- sent_pattern/pipelines/sent_pattern.py
+++ sent_pattern/pipelines/sent_pattern.py
@@ -1,14 +1,24 @@
-from sent_pattern.core.factory.pattern import SentencePattern
+from typing import List, Union
+from sent_pattern.core.factory.pattern import SentencePatternDoc
 from sent_pattern.core.interface.Ipattern import IBaseSentencePattern
 from sent_pattern.tags import create_dep_list, create_elements
 from spacy.tokens import Doc
 from spacy.language import Language
 
 
-def get_sentpattern_type(doc: Doc)-> IBaseSentencePattern:
-    dep_list = create_dep_list(doc)
-    elements = create_elements(dep_list)
-    factory = SentencePattern(elements)
+def get_sentpattern_type(doc: Doc)-> Union[IBaseSentencePattern,List[IBaseSentencePattern]]:
+    factories = []
+    if len(list(doc.sents)) > 1:
+        for d in doc.sents:
+            dep_list = create_dep_list(d)
+            elements = create_elements(dep_list)
+            factory = SentencePatternDoc(elements)
+            factories.append(factory)
+        return factories
+    else:
+        dep_list = create_dep_list(doc)
+        elements = create_elements(dep_list)
+        factory = SentencePatternDoc(elements)
     return factory.pattern_type
 
 @Language.component("sent_pattern")
---
+++ test/test_sents.py
@@ -0,0 +1,31 @@
+import spacy
+import sent_pattern
+from typing import List, Tuple
+import pytest
+
+nlp = spacy.load("en_core_web_lg")
+nlp.add_pipe("span_noun")
+nlp.add_pipe("sent_pattern")
+
+test_data = {
+    "texts": [
+        "The firm obtained that information without users' consent from a researcher who had been allowed by Facebook to deploy an app on the platform which harvested data from millions of its users. She makes me happy.",
+        "I like you"
+        ],
+    "pattern": ["SVO SVOC", "SVO"],
+}
+
+def make_test_data(texts: List[str], answers: List[str])-> List[Tuple[str,str]]:
+    data = []
+    for text, ans in zip(texts, answers):
+        data.append((text, ans))
+    return data
+
+pattern_test_data = make_test_data(test_data["texts"], test_data["pattern"])
+@pytest.mark.parametrize(('text', 'pattern'), pattern_test_data)
+def test_pattern(text:str, pattern:str):
+    doc = nlp(text)
+    ps = pattern.split()
+    patterns = doc._.sentpattern
+    for s, p in zip(ps, patterns):
+        assert p.pattern_type.abbreviation == s
