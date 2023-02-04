35a3f4765c08f1ef18b04fe16f7d21d0a64c0f75
0xpeIpeI <hokumaru12@gmail.com>
2023-02-04 22:38:15 +0900

    public attribute to protected attr

--- sent_pattern/core/elements/patterns.py
+++ sent_pattern/core/elements/patterns.py
@@ -5,11 +5,11 @@ from typing import  Union
 
 
 class FirstSentencePattern(IBaseSentencePattern):
-    span_dict: FirstSpanDictType = dict()
-    span_dict_str: FirstSpanDictTypeStr = dict()
     def __init__(self, subject: Subject, verb: Verb):
         self._subject = subject
         self._verb = verb
+        self._span_dict:FirstSpanDictType = dict()
+        self._span_dict_str:FirstSpanDictTypeStr =dict()
 
     @property
     def subject(self) -> "Subject": return self._subject
@@ -18,20 +18,20 @@ class FirstSentencePattern(IBaseSentencePattern):
     
     @property
     def spans(self) -> FirstSpanDictType:
-        if self._is_element_in_span_dict(self.span_dict):
-            return self.span_dict
-        self.span_dict["S"] = self._subject.span(self._subject.root)
-        self.span_dict["V"] = self._verb.root
-        return self.span_dict
+        if self._is_element_in_span_dict(self._span_dict):
+            return self._span_dict
+        self._span_dict["S"] = self._subject.span(self._subject.root)
+        self._span_dict["V"] = self._verb.root
+        return self._span_dict
     
     @property
     def spans_str(self) -> FirstSpanDictTypeStr:
-        if self._is_element_in_span_dict(self.span_dict_str):
-            return self.span_dict_str
+        if self._is_element_in_span_dict(self._span_dict_str):
+            return self._span_dict_str
         subject_span = self._subject.span(self._subject.root)
-        self.span_dict_str["S"] = self._subject.span_str(subject_span)
-        self.span_dict_str["V"] = self._verb.root.text
-        return self.span_dict_str
+        self._span_dict_str["S"] = self._subject.span_str(subject_span)
+        self._span_dict_str["V"] = self._verb.root.text
+        return self._span_dict_str
     
     def _is_element_in_span_dict(self, dic: Union[FirstSpanDictType,FirstSpanDictTypeStr]) -> bool:
         if len(dic) == 0:
@@ -44,8 +44,6 @@ class FirstSentencePattern(IBaseSentencePattern):
 
 
 class SecondSentencePattern(IBaseSentencePattern):
-    span_dict:SecondSpanDictType = dict()
-    span_dict_str:SecondSpanDictTypeStr = dict()
     def __init__(self, subject: Subject, verb: Verb, adjective: Adjective):
         self._subject = subject
         self._verb = verb
@@ -53,6 +51,8 @@ class SecondSentencePattern(IBaseSentencePattern):
         self._adjective_root = adjective.root
         self._subject_root = subject.root
         self._verb_root = verb.root
+        self._span_dict:SecondSpanDictType = dict()
+        self._span_dict_str:SecondSpanDictTypeStr = dict()
 
     @property
     def subject(self) -> "Subject": return self._subject
@@ -63,23 +63,23 @@ class SecondSentencePattern(IBaseSentencePattern):
     
     @property
     def spans(self) -> SecondSpanDictType:
-        if self._is_element_in_span_dict(self.span_dict):
-            return self.span_dict
-        self.span_dict["S"] = self._subject.span(self._subject_root)
-        self.span_dict["V"] = self._verb_root
-        self.span_dict["C"] = self._adjective.span(self._adjective_root)
-        return self.span_dict
+        if self._is_element_in_span_dict(self._span_dict):
+            return self._span_dict
+        self._span_dict["S"] = self._subject.span(self._subject_root)
+        self._span_dict["V"] = self._verb_root
+        self._span_dict["C"] = self._adjective.span(self._adjective_root)
+        return self._span_dict
     
     @property
     def spans_str(self) -> SecondSpanDictTypeStr:
-        if self._is_element_in_span_dict(self.span_dict_str):
-            return self.span_dict_str
+        if self._is_element_in_span_dict(self._span_dict_str):
+            return self._span_dict_str
         subject_span = self._subject.span(self._subject.root)
         adjective_span = self._adjective.span(self._adjective_root)
-        self.span_dict_str["S"] = self._subject.span_str(subject_span)
-        self.span_dict_str["V"] = self._verb.root.text
-        self.span_dict_str["C"] = self._adjective.span_str(adjective_span)
-        return self.span_dict_str
+        self._span_dict_str["S"] = self._subject.span_str(subject_span)
+        self._span_dict_str["V"] = self._verb.root.text
+        self._span_dict_str["C"] = self._adjective.span_str(adjective_span)
+        return self._span_dict_str
     
     def _is_element_in_span_dict(self, dic:Union[SecondSpanDictType,SecondSpanDictTypeStr]) -> bool:
         if len(dic) == 0:
@@ -93,8 +93,6 @@ class SecondSentencePattern(IBaseSentencePattern):
 
 
 class ThirdSentencePattern(IBaseSentencePattern):
-    span_dict:ThirdSpanDictType = dict()
-    span_dict_str:ThirdSpanDictTypeStr = dict()
     def __init__(self, subject: Subject, verb: Verb, object: RootObject):
         self._subject = subject
         self._verb = verb
@@ -102,6 +100,8 @@ class ThirdSentencePattern(IBaseSentencePattern):
         self._object_root = object.root
         self._subject_root = subject.root
         self._verb_root = verb.root
+        self._span_dict:ThirdSpanDictType = dict()
+        self._span_dict_str:ThirdSpanDictTypeStr = dict()
 
     @property
     def subject(self) -> "Subject": return self._subject
@@ -112,23 +112,23 @@ class ThirdSentencePattern(IBaseSentencePattern):
 
     @property
     def spans(self) -> ThirdSpanDictType:
-        if self._is_element_in_span_dict(self.span_dict):
-            return self.span_dict
-        self.span_dict["S"] = self._subject.span(self._subject_root)
-        self.span_dict["V"] = self._verb_root
-        self.span_dict["O"] = self._object.span(self._object_root)
-        return self.span_dict
+        if self._is_element_in_span_dict(self._span_dict):
+            return self._span_dict
+        self._span_dict["S"] = self._subject.span(self._subject_root)
+        self._span_dict["V"] = self._verb_root
+        self._span_dict["O"] = self._object.span(self._object_root)
+        return self._span_dict
     
     @property
     def spans_str(self) -> ThirdSpanDictTypeStr:
-        if self._is_element_in_span_dict(self.span_dict_str):
-            return self.span_dict_str
+        if self._is_element_in_span_dict(self._span_dict_str):
+            return self._span_dict_str
         subject_span = self._subject.span(self._subject.root)
         object_span = self._object.span(self._object_root)
-        self.span_dict_str["S"] = self._subject.span_str(subject_span)
-        self.span_dict_str["V"] = self._verb_root.text
-        self.span_dict_str["O"] = self._object.span_str(object_span)
-        return self.span_dict_str
+        self._span_dict_str["S"] = self._subject.span_str(subject_span)
+        self._span_dict_str["V"] = self._verb_root.text
+        self._span_dict_str["O"] = self._object.span_str(object_span)
+        return self._span_dict_str
     
     def _is_element_in_span_dict(self, dic:Union[ThirdSpanDictType,ThirdSpanDictTypeStr]) -> bool:
         if len(dic) == 0:
@@ -141,8 +141,6 @@ class ThirdSentencePattern(IBaseSentencePattern):
 
 
 class FourthSentencePattern(IBaseSentencePattern):
-    span_dict:FourthSpanDictType = dict()
-    span_dict_str:FourthSpanDictTypeStr = dict()
     def __init__(self, subject: Subject, verb: Verb, object: RootObject):
         self._subject = subject
         self._verb = verb
@@ -150,6 +148,8 @@ class FourthSentencePattern(IBaseSentencePattern):
         self._object_root = object.root
         self._subject_root = subject.root
         self._verb_root = verb.root
+        self._span_dict:FourthSpanDictType = dict()
+        self._span_dict_str:FourthSpanDictTypeStr = dict()
 
     @property
     def subject(self) -> "Subject": return self._subject
@@ -160,25 +160,25 @@ class FourthSentencePattern(IBaseSentencePattern):
     
     @property
     def spans(self) -> FourthSpanDictType:
-        if self._is_element_in_span_dict(self.span_dict):
-            return self.span_dict
-        self.span_dict["S"] = self._subject.span(self._subject_root)
-        self.span_dict["V"] = self._verb.root
-        self.span_dict["O1"] = self._object.span(self._object_root)[0]
-        self.span_dict["O2"] = self._object.span(self._object_root)[1]
-        return self.span_dict
+        if self._is_element_in_span_dict(self._span_dict):
+            return self._span_dict
+        self._span_dict["S"] = self._subject.span(self._subject_root)
+        self._span_dict["V"] = self._verb.root
+        self._span_dict["O1"] = self._object.span(self._object_root)[0]
+        self._span_dict["O2"] = self._object.span(self._object_root)[1]
+        return self._span_dict
     
     @property
     def spans_str(self) -> FourthSpanDictTypeStr:
-        if self._is_element_in_span_dict(self.span_dict_str):
-            return self.span_dict_str
+        if self._is_element_in_span_dict(self._span_dict_str):
+            return self._span_dict_str
         subject_span = self._subject.span(self._subject.root)
         object_spans = self._object.span(self._object_root)
-        self.span_dict_str["S"] = self._subject.span_str(subject_span)
-        self.span_dict_str["V"] = self._verb.root.text
-        self.span_dict_str["O1"] = self._object.span_str(object_spans)[0]
-        self.span_dict_str["O2"] = self._object.span_str(object_spans)[1]
-        return self.span_dict_str
+        self._span_dict_str["S"] = self._subject.span_str(subject_span)
+        self._span_dict_str["V"] = self._verb.root.text
+        self._span_dict_str["O1"] = self._object.span_str(object_spans)[0]
+        self._span_dict_str["O2"] = self._object.span_str(object_spans)[1]
+        return self._span_dict_str
     
     def _is_element_in_span_dict(self, dic:Union[FourthSpanDictType,FourthSpanDictTypeStr]) -> bool:
         if len(dic) == 0:
@@ -191,8 +191,6 @@ class FourthSentencePattern(IBaseSentencePattern):
 
 
 class FifthSentencePattern(IBaseSentencePattern):
-    span_dict = dict()
-    span_dict_str = dict()
     def __init__(self,
                  subject: Subject,
                  verb: Verb,
@@ -207,6 +205,8 @@ class FifthSentencePattern(IBaseSentencePattern):
         self._subject_root = subject.root
         self._verb_root = verb.root
         self._adjective_root = adjective.root
+        self._span_dict = dict()
+        self._span_dict_str = dict()
 
     @property
     def subject(self) -> "Subject": return self._subject
@@ -219,26 +219,26 @@ class FifthSentencePattern(IBaseSentencePattern):
     
     @property
     def spans(self) -> FifthSpanDictType:
-        if self._is_element_in_span_dict(self.span_dict):
-            return self.span_dict
-        self.span_dict["S"] = self._subject.span(self._subject_root)
-        self.span_dict["V"] = self._verb.root
-        self.span_dict["O"] = self._object.span(self._object_root)
-        self.span_dict["C"] = self._adjective.span(self._adjective_root)
-        return self.span_dict
+        if self._is_element_in_span_dict(self._span_dict):
+            return self._span_dict
+        self._span_dict["S"] = self._subject.span(self._subject_root)
+        self._span_dict["V"] = self._verb.root
+        self._span_dict["O"] = self._object.span(self._object_root)
+        self._span_dict["C"] = self._adjective.span(self._adjective_root)
+        return self._span_dict
     
     @property
     def spans_str(self) -> FifthSpanDictTypeStr:
-        if self._is_element_in_span_dict(self.span_dict_str):
-            return self.span_dict_str
+        if self._is_element_in_span_dict(self._span_dict_str):
+            return self._span_dict_str
         subject_span = self._subject.span(self._subject.root)
         object_spans = self._object.span(self._object_root)
         adjective_span = self._adjective.span(self._adjective_root)
-        self.span_dict_str["S"] = self._subject.span_str(subject_span)
-        self.span_dict_str["V"] = self._verb.root.text
-        self.span_dict_str["O"] = self._object.span_str(object_spans)
-        self.span_dict_str["C"] = self._adjective.span_str(adjective_span)
-        return self.span_dict_str
+        self._span_dict_str["S"] = self._subject.span_str(subject_span)
+        self._span_dict_str["V"] = self._verb.root.text
+        self._span_dict_str["O"] = self._object.span_str(object_spans)
+        self._span_dict_str["C"] = self._adjective.span_str(adjective_span)
+        return self._span_dict_str
 
     def _is_element_in_span_dict(self, dic:Union[FifthSpanDictType, FifthSpanDictTypeStr]) -> bool:
         if len(dic) == 0:
