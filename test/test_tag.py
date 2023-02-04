5fa2f8c33509e5ffea9100ff866ec69476929146
0xpeIpeI <hokumaru12@gmail.com>
2023-02-04 22:58:57 +0900

    delete test

--- test/test_tag.py
+++ test/test_tag.py
@@ -8,20 +8,3 @@ nlp.add_pipe("sent_pattern")
 
 def test_pipe_added_to_nlp():
     assert nlp.pipe_names[-1] == "sent_pattern"
-
-
-def test_dep_list_get_root_verb():
-    text = "he gives me something"
-    doc = nlp(text)
-    dep_list = tags.create_dep_list(doc)
-    root_verb = dep_list["ROOT"]
-    assert root_verb[0].text == "gives"
-
-
-def test_sent_pattern():
-    text = "he gives me something"
-    doc = nlp(text)
-    dep_list = tags.create_dep_list(doc)
-    elements = tags.create_elements(dep_list)
-    pattern = tags.create_sent_pattern(elements)
-    assert pattern.type.subject.root.text == "he"
