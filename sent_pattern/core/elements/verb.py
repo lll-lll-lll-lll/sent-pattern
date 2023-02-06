57cef9dfca6b6ce73e065e5706bba423c8cd0878
0xpeIpeI <hokumaru12@gmail.com>
2023-02-06 20:59:39 +0900

    delete RootVerb class

--- sent_pattern/core/elements/verb.py
+++ sent_pattern/core/elements/verb.py
@@ -1,25 +1,9 @@
-from typing import Any, List
+from typing import Any, List, Optional
 from spacy.tokens import Token
 from sent_pattern.core.interface.Ielement import IRootElement
 
 
-class RootVerb:
-    def __init__(self, verb: Token):
-        self.__root = verb
-        if not verb:
-            raise AttributeError.message("root verb doesn't exist.")
-
-    @property
-    def root(self) -> Token:
-        return self.__root
-
-    def __str__(self) -> str:
-        return self.root.text
-
-
 class Verb:
-    DEP = []
-
     def __init__(self, dep_list):
         self._dep_list = dep_list
         self._verb_root = self._get_root_verb()
@@ -33,6 +17,8 @@ class Verb:
         child = [child for child in self._dep_list["ROOT"][0].children]
         return child
 
-    def _get_root_verb(self) -> Token:
-        root = RootVerb(self._dep_list["ROOT"][0]).root
+    def _get_root_verb(self) -> Optional[Token]:
+        root = self._dep_list["ROOT"][0]
+        if not root:
+            return
         return root
